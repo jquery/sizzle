ea143edafa0243dafc46f51f4216bb0850040fad
John Resig <jeresig@gmail.com>
2008-11-01 07:08:45 -0700

    Added more features to the jQuery-specific Sizzle code - now serves as a complete drop-in replacement for the existing src/selector.js file in jQuery.

--- libs/jquery.js
+++ libs/jquery.js
@@ -13,4 +13,50 @@ Sizzle.selectors.filters.visible = function(elem){
 		jQuery.css(elem, "visibility") !== "hidden";
 };
 
+jQuery.multiFilter = function( expr, elems, not ) {
+	if ( not ) {
+		return jQuery.multiFilter( ":not(" + expr + ")", elems );
+	}
+
+	var exprs = expr.split(/\s*,\s*/), cur = [];
+
+	for ( var i = 0; i < exprs.length; i++ ) {
+		cur = jQuery.merge( cur, jQuery.filter( exprs[i], elems ) );
+	}
+
+	return cur;
+};
+
+jQuery.dir = function( elem, dir ){
+	var matched = [], cur = elem[dir];
+	while ( cur && cur != document ) {
+		if ( cur.nodeType == 1 )
+			matched.push( cur );
+		cur = cur[dir];
+	}
+	return matched;
+};
+
+jQuery.nth = function(cur, result, dir, elem){
+	result = result || 1;
+	var num = 0;
+
+	for ( ; cur; cur = cur[dir] )
+		if ( cur.nodeType == 1 && ++num == result )
+			break;
+
+	return cur;
+};
+
+jQuery.sibling = function(n, elem){
+	var r = [];
+
+	for ( ; n; n = n.nextSibling ) {
+		if ( n.nodeType == 1 && n != elem )
+			r.push( n );
+	}
+
+	return r;
+};
+
 return;
