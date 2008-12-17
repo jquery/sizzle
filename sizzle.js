d01a9e283d35b5ada3a9e810f3fe2a71895682b9
John Resig <jeresig@gmail.com>
2008-12-16 20:01:27 -0500

    Tweaked the copyright header.

--- sizzle.js
+++ sizzle.js
@@ -1,6 +1,6 @@
 /*
- * Sizzle CSS Selector Engine
- *  Copyright 2008, John Resig (http://ejohn.org/)
+ * Sizzle CSS Selector Engine - v0.9
+ *  Copyright 2009, John Resig (http://ejohn.org/)
  *  released under the MIT License
  */
 (function(){
