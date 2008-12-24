0176d850ca4ed8e6edc4c44d62cd6ecd09d810b9
Cheah Chu Yeow <chuyeow@gmail.com>
2008-12-24 13:15:11 +0800

    Fix test broken by addition of form#name-tests in a previous commit.
    Signed-off-by: John Resig <jeresig@gmail.com>

--- test/unit/core.js
+++ test/unit/core.js
@@ -1266,7 +1266,7 @@ test("siblings([String])", function() {
 	isSet( jQuery("#en").siblings().get(), q("sndp", "sap"), "Check for siblings" );
 	isSet( jQuery("#sndp").siblings(":has(code)").get(), q("sap"), "Check for filtered siblings (has code child element)" );
 	isSet( jQuery("#sndp").siblings(":has(a)").get(), q("en", "sap"), "Check for filtered siblings (has anchor child element)" );
-	isSet( jQuery("#foo").siblings("form, b").get(), q("form", "lengthtest", "testForm", "floatTest"), "Check for multiple filters" );
+	isSet( jQuery("#foo").siblings("form, b").get(), q("form", "lengthtest", "name-tests", "testForm", "floatTest"), "Check for multiple filters" );
 	isSet( jQuery("#en, #sndp").siblings().get(), q("sndp", "sap", "en"), "Check for unique results from siblings" );
 });
 
