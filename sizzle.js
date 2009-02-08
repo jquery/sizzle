158458ee246a7bfde6a486a8f09bc385f3920251
LosTFx <fabio.buffoni@bitmaster.it>
2009-02-09 00:44:31 +0800

    Removed elem.nodeType test from dirNodeCheck and dirCheck
    Big general speed UP
    
    Signed-off-by: John Resig <jeresig@gmail.com>

--- sizzle.js
+++ sizzle.js
@@ -777,7 +777,7 @@ function dirNodeCheck( dir, cur, doneName, checkSet, nodeCheck, isXML ) {
 			elem = elem[dir];
 			var match = false;
 
-			while ( elem && elem.nodeType ) {
+			while ( elem ) {
 				var done = elem[doneName];
 				if ( done ) {
 					match = checkSet[ done ];
@@ -807,7 +807,7 @@ function dirCheck( dir, cur, doneName, checkSet, nodeCheck, isXML ) {
 			elem = elem[dir];
 			var match = false;
 
-			while ( elem && elem.nodeType ) {
+			while ( elem ) {
 				if ( elem[doneName] ) {
 					match = checkSet[ elem[doneName] ];
 					break;
