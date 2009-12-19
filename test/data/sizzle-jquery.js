0032944eb5893cc732dac3df20a7617f75a027c5
jeresig <jeresig@gmail.com>
2009-12-19 18:40:58 -0500

    Make sure that we're not trying to pull in things that don't exist, durr.

--- test/data/sizzle-jquery.js
+++ test/data/sizzle-jquery.js
@@ -2,6 +2,3 @@ jQuery.find = Sizzle;
 jQuery.expr = Sizzle.selectors;
 jQuery.expr[":"] = jQuery.expr.filters;
 jQuery.unique = Sizzle.uniqueSort;
-jQuery.getText = getText;
-jQuery.isXMLDoc = isXML;
-jQuery.contains = contains;
