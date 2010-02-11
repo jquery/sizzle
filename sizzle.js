4846c48e13ba6b5720c84f23cad1a9a48d7b5557
jeresig <jeresig@gmail.com>
2010-02-11 18:45:25 -0500

    Add a fix to makeArray for the Blackberry. Thanks to Filip Maj for the suggestion and testing.

--- sizzle.js
+++ sizzle.js
@@ -710,8 +710,10 @@ var makeArray = function(array, results) {
 
 // Perform a simple check to determine if the browser is capable of
 // converting a NodeList to an array using builtin methods.
+// Also verifies that the returned array holds DOM nodes
+// (which is not the case in the Blackberry browser)
 try {
-	Array.prototype.slice.call( document.documentElement.childNodes, 0 );
+	Array.prototype.slice.call( document.documentElement.childNodes, 0 )[0].nodeType;
 
 // Provide a fallback method if it does not work
 } catch(e){
