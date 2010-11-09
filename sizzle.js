jquery/sizzle

commit aa6076c6a2b79acd2f8c67592f91206bbcb08f03

Ensure that unquoted attribute selectors are quoted (allowing them to go into qSA/matchesSelector properly). Fixes #7216.

sizzle.js
@@ -1082,6 +1082,9 @@ if ( document.querySelectorAll ) {
 		Sizzle = function( query, context, extra, seed ) {
 			context = context || document;
 
+			// Make sure that attribute selectors are quoted
+			query = query.replace(/=\s*([^'"\]]*)\s*\]/g, "='$1']");
+
 			// Only use querySelectorAll on non-XML documents
 			// (ID selectors don't work in non-HTML documents)
 			if ( !seed && !Sizzle.isXML(context) ) {
@@ -1142,6 +1145,9 @@ if ( document.querySelectorAll ) {
 
 	if ( matches ) {
 		Sizzle.matchesSelector = function( node, expr ) {
+			// Make sure that attribute selectors are quoted
+			expr = expr.replace(/=\s*([^'"\]]*)\s*\]/g, "='$1']");
+
 			if ( !Sizzle.isXML( node ) ) {
 				try { 
 					if ( pseudoWorks || !Expr.match.PSEUDO.test( expr ) && !/!=/.test( expr ) ) {

test/unit/selector.js
@@ -237,7 +237,7 @@ test("child and adjacent", function() {
 });
 
 test("attributes", function() {
-	expect(35);
+	expect(39);
 	t( "Attribute Exists", "a[title]", ["google"] );
 	t( "Attribute Exists", "*[title]", ["google"] );
 	t( "Attribute Exists", "[title]", ["google"] );
@@ -275,8 +275,16 @@ test("attributes", function() {
 	t( "Attribute Contains", "a[href *= 'google']", ["google","groups"] );
 	t( "Attribute Is Not Equal", "#ap a[hreflang!='en']", ["google","groups","anchor1"] );
 
-	var opt = document.getElementById("option1a");
-	ok( (window.Sizzle || window.jQuery.find).matchesSelector( opt, "[id*=option1][type!=checkbox]" ), "Attribute Is Not Equal Matches" );
+	var opt = document.getElementById("option1a"),
+		match = (window.Sizzle || window.jQuery.find).matchesSelector;
+
+	opt.setAttribute("test", "");
+
+	ok( match( opt, "[id*=option1][type!=checkbox]" ), "Attribute Is Not Equal Matches" );
+	ok( match( opt, "[id*=option1]" ), "Attribute With No Quotes Contains Matches" );
+	ok( match( opt, "[test=]" ), "Attribute With No Quotes No Content Matches" );
+	ok( match( opt, "[id=option1a]" ), "Attribute With No Quotes Equals Matches" );
+	ok( match( document.getElementById("simon1"), "a[href*=#]" ), "Attribute With No Quotes Href Contains Matches" );
 
 	t("Empty values", "#select1 option[value='']", ["option1a"]);
 	t("Empty values", "#select1 option[value!='']", ["option1b","option1c","option1d"]);

commit 112ff172e06c92fa87c3362baad13153302f7359

Fix jslint failures caused by aa6076c6a2b79acd2f8c.

sizzle.js
@@ -1083,7 +1083,7 @@ if ( document.querySelectorAll ) {
 			context = context || document;
 
 			// Make sure that attribute selectors are quoted
-			query = query.replace(/=\s*([^'"\]]*)\s*\]/g, "='$1']");
+			query = query.replace(/\=\s*([^'"\]]*)\s*\]/g, "='$1']");
 
 			// Only use querySelectorAll on non-XML documents
 			// (ID selectors don't work in non-HTML documents)
@@ -1146,7 +1146,7 @@ if ( document.querySelectorAll ) {
 	if ( matches ) {
 		Sizzle.matchesSelector = function( node, expr ) {
 			// Make sure that attribute selectors are quoted
-			expr = expr.replace(/=\s*([^'"\]]*)\s*\]/g, "='$1']");
+			expr = expr.replace(/\=\s*([^'"\]]*)\s*\]/g, "='$1']");
 
 			if ( !Sizzle.isXML( node ) ) {
 				try { 