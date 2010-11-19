3332b30d0faacf9cef5ee537b0f6651921dae4b8
Colin Snover <github.com@zetafleet.com>
2010-11-18 23:13:49 -0600

    Use bitwise operators instead of modulo operators for :odd and :even pseudo-selectors to improve performance.

--- sizzle.js
+++ sizzle.js
@@ -646,11 +646,11 @@ var Expr = Sizzle.selectors = {
 		},
 
 		even: function( elem, i ) {
-			return i % 2 === 0;
+			return (i & 1) === 0;
 		},
 
 		odd: function( elem, i ) {
-			return i % 2 === 1;
+			return (i & 1) === 1;
 		},
 
 		lt: function( elem, i, match ) {
