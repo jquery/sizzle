c29c8482773dddc8662a1dbd27866524fa5e8082
Anton M <obhvsbypqghgc@gmail.com>
2011-01-28 01:23:28 +0100

    Fix the handling of escaped attribute names/values for non qSA browsers. Fixes #3729. Fixes #6428.

--- sizzle.js
+++ sizzle.js
@@ -531,14 +531,14 @@ var Expr = Sizzle.selectors = {
 		},
 
 		ATTR: function( match, curLoop, inplace, result, not, isXML ) {
-			var name = match[1].replace(/\\/g, "");
+			var name = match[1] = match[1].replace(/\\/g, "");
 			
 			if ( !isXML && Expr.attrMap[name] ) {
 				match[1] = Expr.attrMap[name];
 			}
 
 			// Handle if an un-quoted value was used
-			match[4] = match[4] || match[5] || "";
+			match[4] = ( match[4] || match[5] || "" ).replace(/\\/g, "");
 
 			if ( match[2] === "~=" ) {
 				match[4] = " " + match[4] + " ";
