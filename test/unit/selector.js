jquery/sizzle

commit 8b3468716e629fe2340fc8e407abe8a389b0d4f5

Follow-up to fix for #7452. Enable use of qSA in most cases.
This "optimization" will allow the use of qSA even for Element-rooted
hierarchy-selectors. Only when the Element-root has no parent node
it falls back to oldSizzle.

sizzle.js
@@ -1108,14 +1108,19 @@ if ( document.querySelectorAll ) {
 				// IE 8 doesn't work on object elements
 				} else if ( context.nodeType === 1 && context.nodeName.toLowerCase() !== "object" ) {
 					var old = context.getAttribute( "id" ),
-						nid = old || id;
+						nid = old || id,
+						hasParent = context.parentNode,
+						relativeHierarchySelector = /^\s*[+~]/.test( query );
 
 					if ( !old ) {
 						context.setAttribute( "id", nid );
 					}
+					if ( relativeHierarchySelector && hasParent ) {
+						context = context.parentNode;
+					}
 
 					try {
-						if ( !/^\s*[+~]/.test( query ) ) {
+						if ( !relativeHierarchySelector || hasParent ) {
 							return makeArray( context.querySelectorAll( "#" + nid + " " + query ), extra );
 						}
 

test/unit/selector.js
@@ -213,7 +213,7 @@ test("multiple", function() {
 });
 
 test("child and adjacent", function() {
-	expect(29);
+	expect(31);
 	t( "Child", "p > a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p> a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p >a", ["simon1","google","groups","mark","yahoo","simon"] );
@@ -237,6 +237,9 @@ test("child and adjacent", function() {
 	t( "Element Preceded By", "#siblingfirst ~ em", ["siblingnext"] );
 	same( jQuery("#siblingfirst").find("~ em").get(), q("siblingnext"), "Element Preceded By with a context." );
 	same( jQuery("#siblingfirst").find("+ em").get(), q("siblingnext"), "Element Directly Preceded By with a context." );
+	var a = jQuery("<div id='foo'></div><p id='bar'></p><p id='bar2'></p>");
+	same( jQuery("~ p", a[0]).get(), [a[1], a[2]], "Detached Element Directly Preceded By with a context." );
+	same( jQuery("+ p", a[0]).get(), [a[1]], "Detached Element Preceded By with a context." );
 
 	t( "Verify deep class selector", "div.blah > p > a", [] );
 

commit ef19279f54ba49242c6461d47577c703f4f4e80e

Fixes #8310. Make sure that sibling selectors (+,~) in some edge cases don't leave modification behind in the DOM.
Follow up to 8b3468716e629fe2340fc8e407abe8a389b0d4f5 which caused Sizzle to sometimes
leave modifications behind in the DOM and remove the id attribute on the wrong element.

sizzle.js
@@ -1157,7 +1157,8 @@ if ( document.querySelectorAll ) {
 				// and working up from there (Thanks to Andrew Dupont for the technique)
 				// IE 8 doesn't work on object elements
 				} else if ( context.nodeType === 1 && context.nodeName.toLowerCase() !== "object" ) {
-					var old = context.getAttribute( "id" ),
+					var oldContext = context,
+						old = context.getAttribute( "id" ),
 						nid = old || id,
 						hasParent = context.parentNode,
 						relativeHierarchySelector = /^\s*[+~]/.test( query );
@@ -1179,7 +1180,7 @@ if ( document.querySelectorAll ) {
 					} catch(pseudoError) {
 					} finally {
 						if ( !old ) {
-							context.removeAttribute( "id" );
+							oldContext.removeAttribute( "id" );
 						}
 					}
 				}

test/unit/selector.js
@@ -228,7 +228,7 @@ test("multiple", function() {
 });
 
 test("child and adjacent", function() {
-	expect(29);
+	expect(33);
 	t( "Child", "p > a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p> a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p >a", ["simon1","google","groups","mark","yahoo","simon"] );
@@ -252,6 +252,11 @@ test("child and adjacent", function() {
 	t( "Element Preceded By", "#siblingfirst ~ em", ["siblingnext"] );
 	same( jQuery("#siblingfirst").find("~ em").get(), q("siblingnext"), "Element Preceded By with a context." );
 	same( jQuery("#siblingfirst").find("+ em").get(), q("siblingnext"), "Element Directly Preceded By with a context." );
+
+	equal( jQuery("#listWithTabIndex").length, 1, "Parent div for next test is found via ID (#8310)" );
+	equal( jQuery("#listWithTabIndex li:eq(2) ~ li").length, 1, "Find by general sibling combinator (#8310)" );
+	equal( jQuery("#__sizzle__").length, 0, "Make sure the temporary id assigned by sizzle is cleared out (#8310)" );
+	equal( jQuery("#listWithTabIndex").length, 1, "Parent div for previous test is still found via ID (#8310)" );
 
 	t( "Verify deep class selector", "div.blah > p > a", [] );
 