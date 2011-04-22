dd8669bc9d685476e5d953421fd50e919aa07228
timmywil <tim.willison@thisismedium.com>
2011-04-22 10:34:47 -0400

    Add uppercase attr type='TEXT' support to :text filter. Fixes jQuery bug #8943

--- sizzle.js
+++ sizzle.js
@@ -622,10 +622,11 @@ var Expr = Sizzle.selectors = {
 		},
 
 		text: function( elem ) {
-			var attr = elem.getAttribute( "type" ), type = elem.type;
+			var attr = elem.getAttribute("type"), type = elem.type;
 			// IE6 and 7 will map elem.type to 'text' for new HTML5 types (search, etc) 
 			// use getAttribute instead to test this case
-			return elem.nodeName.toLowerCase() === "input" && "text" === type && ( attr === type || attr === null );
+			return elem.nodeName.toLowerCase() === "input" && "text" === type &&
+				( (attr && attr.toLowerCase() === type) || attr === null );
 		},
 
 		radio: function( elem ) {
--- test/unit/selector.js
+++ test/unit/selector.js
@@ -197,7 +197,7 @@ test("name", function() {
 	t( "Name selector non-input", "[name=div]", ["fadein"] );
 	t( "Name selector non-input", "*[name=iframe]", ["iframe"] );
 
-	t( "Name selector for grouped input", "input[name='types[]']", ["types_all", "types_anime", "types_movie"] )
+	t( "Name selector for grouped input", "input[name='types[]']", ["types_all", "types_anime", "types_movie"] );
 
 	same( jQuery("#form").find("input[name=action]").get(), q("text1"), "Name selector within the context of another element" );
 	same( jQuery("#form").find("input[name='foo[bar]']").get(), q("hidden2"), "Name selector for grouped form element within the context of another element" );
@@ -533,19 +533,19 @@ test("pseudo - visibility", function() {
 test("pseudo - form", function() {
 	expect(8);
 
-	var implied = jQuery('<input id="impliedText"/>').appendTo("#form");
+	var extraTexts = jQuery('<input id="impliedText"/><input id="capitalText" type="TEXT">').appendTo("#form");
 
-	t( "Form element :input", "#form :input", ["text1", "text2", "radio1", "radio2", "check1", "check2", "hidden1", "hidden2", "name", "search", "button", "area1", "select1", "select2", "select3", "select4", "select5", "impliedText"] );
+	t( "Form element :input", "#form :input", ["text1", "text2", "radio1", "radio2", "check1", "check2", "hidden1", "hidden2", "name", "search", "button", "area1", "select1", "select2", "select3", "select4", "select5", "impliedText", "capitalText"] );
 	t( "Form element :radio", "#form :radio", ["radio1", "radio2"] );
 	t( "Form element :checkbox", "#form :checkbox", ["check1", "check2"] );
-	t( "Form element :text", "#form :text", ["text1", "text2", "hidden2", "name", "impliedText"] );
+	t( "Form element :text", "#form :text", ["text1", "text2", "hidden2", "name", "impliedText", "capitalText"] );
 	t( "Form element :radio:checked", "#form :radio:checked", ["radio2"] );
 	t( "Form element :checkbox:checked", "#form :checkbox:checked", ["check1"] );
 	t( "Form element :radio:checked, :checkbox:checked", "#form :radio:checked, #form :checkbox:checked", ["radio2", "check1"] );
 
 	t( "Selected Option Element", "#form option:selected", ["option1a","option2d","option3b","option3c","option4b","option4c","option4d","option5a"] );
 
-	implied.remove();
+	extraTexts.remove();
 });
 
 test("disconnected nodes", function() {
