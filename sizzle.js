77399b2833af81bdfc5dee6c41c93b5586350512
Mike Taylor <miketaylr@gmail.com>
2011-10-14 14:10:10 -0400

    adding in oMatches Selector (supported since Opera 11.50)

--- sizzle.js
+++ sizzle.js
@@ -1243,7 +1243,7 @@ if ( document.querySelectorAll ) {
 
 (function(){
 	var html = document.documentElement,
-		matches = html.matchesSelector || html.mozMatchesSelector || html.webkitMatchesSelector || html.msMatchesSelector;
+		matches = html.matchesSelector || html.mozMatchesSelector || html.webkitMatchesSelector || html.msMatchesSelector || html.oMatchesSelector;
 
 	if ( matches ) {
 		// Check to see if it's possible to do matchesSelector
