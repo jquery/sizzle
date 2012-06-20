c53fb710040ba25c000f7daed0628683961f08db
timmywil <timmywillisn@gmail.com>
2012-06-19 20:09:24 -0400

    Add back matchesSelector support (with all browser versions). The speed is too great to give up.
    Related:
    http://jsperf.com/jquery-id-init/3

--- sizzle.js
+++ sizzle.js
@@ -146,7 +146,7 @@ var document = window.document,
 var Sizzle = function( selector, context, results, seed ) {
 	results = results || [];
 	context = context || document;
-	var match, elem, contextXML,
+	var match, elem, contextXML, m,
 		nodeType = context.nodeType;
 
 	if ( nodeType !== 1 && nodeType !== 9 ) {
@@ -162,15 +162,15 @@ var Sizzle = function( selector, context, results, seed ) {
 	if ( !contextXML && !seed ) {
 		if ( (match = rquickExpr.exec( selector )) ) {
 			// Speed-up: Sizzle("#ID")
-			if ( match[1] ) {
+			if ( (m = match[1]) ) {
 				if ( nodeType === 9 ) {
-					elem = context.getElementById( match[1] );
+					elem = context.getElementById( m );
 					// Check parentNode to catch when Blackberry 4.6 returns
 					// nodes that are no longer in the document #6963
 					if ( elem && elem.parentNode ) {
 						// Handle the case where IE, Opera, and Webkit return items
 						// by name instead of ID
-						if ( elem.id === match[1] ) {
+						if ( elem.id === m ) {
 							return makeArray( [ elem ], results );
 						}
 					} else {
@@ -178,22 +178,19 @@ var Sizzle = function( selector, context, results, seed ) {
 					}
 				} else {
 					// Context is not a document
-					if ( context.ownerDocument && (elem = context.ownerDocument.getElementById( match[1] )) &&
-						contains( context, elem ) && elem.id === match[1] ) {
+					if ( context.ownerDocument && (elem = context.ownerDocument.getElementById( m )) &&
+						contains( context, elem ) && elem.id === m ) {
 						return makeArray( [ elem ], results );
 					}
 				}
 
 			// Speed-up: Sizzle("TAG")
 			} else if ( match[2] ) {
-				// Speed-up: Sizzle("body")
-				if ( selector === "body" && context.body ) {
-					return makeArray( [ context.body ], results );
-				}
 				return makeArray( context.getElementsByTagName( selector ), results );
+
 			// Speed-up: Sizzle(".CLASS")
-			} else if ( assertUsableClassName && match[3] && context.getElementsByClassName ) {
-				return makeArray( context.getElementsByClassName( match[3] ), results );
+			} else if ( assertUsableClassName && (m = match[3]) && context.getElementsByClassName ) {
+				return makeArray( context.getElementsByClassName( m ), results );
 			}
 		}
 	}
@@ -353,120 +350,12 @@ var select = function( selector, context, results, seed, contextXML ) {
 	return results;
 };
 
-if ( document.querySelectorAll ) {
-	(function(){
-		var oldSelect = select,
-			id = "__sizzle__",
-			rdivision = /[^\\],/g,
-			rrelativeHierarchy = /^\s*[+~]/,
-			rapostrophe = /'/g,
-			// Build QSA regex
-			// Regex strategy adopted from Diego Perini
-			rbuggyQSA = [];
-
-		assert(function( div ) {
-			div.innerHTML = "<select><option selected></option></select>";
-
-			// IE8 - Some boolean attributes are not treated correctly
-			if ( !div.querySelectorAll("[selected]").length ) {
-				rbuggyQSA.push("\\[[\\x20\\t\\n\\r\\f]*(?:checked|disabled|ismap|multiple|readonly|selected|value)");
-			}
-
-			// Webkit/Opera - :checked should return selected option elements
-			// http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#checked
-			// IE8 throws error here (do not put tests after this one)
-			if ( !div.querySelectorAll(":checked").length ) {
-				rbuggyQSA.push(":checked");
-			}
-		});
-
-		assert(function( div ) {
-
-			// Opera 10/IE - ^= $= *= and empty values
-			div.innerHTML = "<p class=''></p>";
-			// Should not select anything
-			if ( div.querySelectorAll("[class^='']").length ) {
-				rbuggyQSA.push("[*^$]=[\\x20\\t\\n\\r\\f]*(?:\"\"|'')");
-			}
-
-			// FF 3.5 - :enabled/:disabled and hidden elements (hidden elements are still enabled)
-			// IE8 throws error here (do not put tests after this one)
-			div.innerHTML = "<input type='hidden'>";
-			if ( !div.querySelectorAll(":enabled").length ) {
-				rbuggyQSA.push(":enabled", ":disabled");
-			}
-		});
-
-		rbuggyQSA = rbuggyQSA.length && new RegExp( rbuggyQSA.join("|") );
-
-		select = function( selector, context, results, seed, contextXML ) {
-			// Only use querySelectorAll when not filtering,
-			// when this is not xml,
-			// and when no QSA bugs apply
-			if ( !seed && !contextXML && (!rbuggyQSA || !rbuggyQSA.test( selector )) ) {
-				if ( context.nodeType === 9 ) {
-					try {
-						return makeArray( context.querySelectorAll( selector ), results );
-					} catch(qsaError) {}
-				// qSA works strangely on Element-rooted queries
-				// We can work around this by specifying an extra ID on the root
-				// and working up from there (Thanks to Andrew Dupont for the technique)
-				// IE 8 doesn't work on object elements
-				} else if ( context.nodeType === 1 && context.nodeName.toLowerCase() !== "object" ) {
-					var newSelector,
-						oldContext = context,
-						old = context.getAttribute( "id" ),
-						nid = old || id,
-						parent = context.parentNode,
-						relativeHierarchySelector = rrelativeHierarchy.test( selector );
-
-					if ( !old ) {
-						context.setAttribute( "id", nid );
-					} else {
-						nid = nid.replace( rapostrophe, "\\$&" );
-					}
-
-					if ( relativeHierarchySelector && parent ) {
-						context = parent;
-					}
-
-					try {
-						if ( !relativeHierarchySelector || parent ) {
-							nid = "[id='" + nid + "'] ";
-							newSelector = nid + selector.replace( rdivision, "$&" + nid );
-							return makeArray( context.querySelectorAll( newSelector ), results );
-						}
-					} catch(qsaError) {
-					} finally {
-						if ( !old ) {
-							oldContext.removeAttribute( "id" );
-						}
-					}
-				}
-			}
-
-			return oldSelect( selector, context, results, seed, contextXML );
-		};
-	})();
-}
-
-Sizzle.attr = function( elem, name ) {
-	if ( Expr.attrHandle[ name ] ) {
-		return Expr.attrHandle[ name ]( elem );
-	}
-	if ( assertAttributes || isXML( elem ) ) {
-		return elem.getAttribute( name );
-	}
-	var attr = (elem.attributes || {})[ name ];
-	return attr && attr.specified ? attr.value : null;
-};
-
 Sizzle.matches = function( expr, set ) {
 	return Sizzle( expr, null, null, set );
 };
 
-Sizzle.matchesSelector = function( node, expr ) {
-	return Sizzle( expr, null, null, [ node ] ).length > 0;
+Sizzle.matchesSelector = function( elem, expr ) {
+	return Sizzle( expr, null, null, [ elem ] ).length > 0;
 };
 
 Sizzle.find = function( expr, context, contextXML ) {
@@ -627,10 +516,162 @@ Sizzle.filter = function( expr, set, inplace, not ) {
 	return set;
 };
 
+Sizzle.attr = function( elem, name ) {
+	if ( Expr.attrHandle[ name ] ) {
+		return Expr.attrHandle[ name ]( elem );
+	}
+	if ( assertAttributes || isXML( elem ) ) {
+		return elem.getAttribute( name );
+	}
+	var attr = (elem.attributes || {})[ name ];
+	return attr && attr.specified ? attr.value : null;
+};
+
 Sizzle.error = function( msg ) {
 	throw new Error( "Syntax error, unrecognized expression: " + msg );
 };
 
+if ( document.querySelectorAll ) {
+	(function(){
+		var disconnectedMatch,
+			oldSelect = select,
+			id = "__sizzle__",
+			rdivision = /[^\\],/g,
+			rrelativeHierarchy = /^\s*[+~]/,
+			rapostrophe = /'/g,
+			rattributeQuotes = /\=\s*([^'"\]]*)\s*\]/g,
+			rbuggyQSA = [],
+			rbuggyMatches = [],
+			matches = docElem.matchesSelector ||
+				docElem.mozMatchesSelector ||
+				docElem.webkitMatchesSelector ||
+				docElem.oMatchesSelector ||
+				docElem.msMatchesSelector;
+
+		// Build QSA regex
+		// Regex strategy adopted from Diego Perini
+		assert(function( div ) {
+			div.innerHTML = "<select><option selected></option></select>";
+
+			// IE8 - Some boolean attributes are not treated correctly
+			if ( !div.querySelectorAll("[selected]").length ) {
+				rbuggyQSA.push("\\[[\\x20\\t\\n\\r\\f]*(?:checked|disabled|ismap|multiple|readonly|selected|value)");
+			}
+
+			// Webkit/Opera - :checked should return selected option elements
+			// http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#checked
+			// IE8 throws error here (do not put tests after this one)
+			if ( !div.querySelectorAll(":checked").length ) {
+				rbuggyQSA.push(":checked");
+			}
+		});
+
+		assert(function( div ) {
+
+			// Opera 10/IE - ^= $= *= and empty values
+			div.innerHTML = "<p class=''></p>";
+			// Should not select anything
+			if ( div.querySelectorAll("[class^='']").length ) {
+				rbuggyQSA.push("[*^$]=[\\x20\\t\\n\\r\\f]*(?:\"\"|'')");
+			}
+
+			// FF 3.5 - :enabled/:disabled and hidden elements (hidden elements are still enabled)
+			// IE8 throws error here (do not put tests after this one)
+			div.innerHTML = "<input type='hidden'>";
+			if ( !div.querySelectorAll(":enabled").length ) {
+				rbuggyQSA.push(":enabled", ":disabled");
+			}
+		});
+
+		rbuggyQSA = rbuggyQSA.length && new RegExp( rbuggyQSA.join("|") );
+
+		select = function( selector, context, results, seed, contextXML ) {
+			// Only use querySelectorAll when not filtering,
+			// when this is not xml,
+			// and when no QSA bugs apply
+			if ( !seed && !contextXML && (!rbuggyQSA || !rbuggyQSA.test( selector )) ) {
+				if ( context.nodeType === 9 ) {
+					try {
+						return makeArray( context.querySelectorAll( selector ), results );
+					} catch(qsaError) {}
+				// qSA works strangely on Element-rooted queries
+				// We can work around this by specifying an extra ID on the root
+				// and working up from there (Thanks to Andrew Dupont for the technique)
+				// IE 8 doesn't work on object elements
+				} else if ( context.nodeType === 1 && context.nodeName.toLowerCase() !== "object" ) {
+					var newSelector,
+						oldContext = context,
+						old = context.getAttribute( "id" ),
+						nid = old || id,
+						parent = context.parentNode,
+						relativeHierarchySelector = rrelativeHierarchy.test( selector );
+
+					if ( !old ) {
+						context.setAttribute( "id", nid );
+					} else {
+						nid = nid.replace( rapostrophe, "\\$&" );
+					}
+
+					if ( relativeHierarchySelector && parent ) {
+						context = parent;
+					}
+
+					try {
+						if ( !relativeHierarchySelector || parent ) {
+							nid = "[id='" + nid + "'] ";
+							newSelector = nid + selector.replace( rdivision, "$&" + nid );
+							return makeArray( context.querySelectorAll( newSelector ), results );
+						}
+					} catch(qsaError) {
+					} finally {
+						if ( !old ) {
+							oldContext.removeAttribute( "id" );
+						}
+					}
+				}
+			}
+
+			return oldSelect( selector, context, results, seed, contextXML );
+		};
+
+		if ( matches ) {
+			assert(function( div ) {
+				// Check to see if it's possible to do matchesSelector
+				// on a disconnected node (IE 9 fails this)
+				disconnectedMatch = matches.call( div, "div" );
+				try {
+					// This should fail with an exception
+					// Gecko does not error, returns false instead
+					matches.call( div, "[test!='']:sizzle" );
+					rbuggyMatches.push( Expr.match.PSEUDO );
+				} catch ( e ) {}
+			});
+
+			rbuggyMatches = rbuggyMatches.length && new RegExp( rbuggyMatches.join("|") );
+
+			Sizzle.matchesSelector = function( elem, expr ) {
+				// Make sure that attribute selectors are quoted
+				expr = expr.replace( rattributeQuotes, "='$1']" );
+
+				if ( !isXML( elem ) && (!rbuggyMatches || rbuggyMatches.test( expr )) && (!rbuggyQSA || !rbuggyQSA.test( expr )) ) {
+					try {
+						var ret = matches.call( elem, expr );
+
+						// IE 9's matchesSelector returns false on disconnected nodes
+						if ( ret || !disconnectedMatch ||
+								// As well, disconnected nodes are said to be in a document
+								// fragment in IE 9
+								elem.document && elem.document.nodeType !== 11 ) {
+							return ret;
+						}
+					} catch(e) {}
+				}
+
+				return Sizzle( expr, null, null, [ elem ] ).length > 0;
+			};
+		}
+	})();
+}
 
 // Slice is no longer used
 // Results is expected to be an array or undefined
