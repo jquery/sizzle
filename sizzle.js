fa5affe33c9581d13f08adb7bbb7b575373f7c9a
timmywil <timmywillisn@gmail.com>
2012-06-26 16:55:02 -0400

    Update copyright year

--- sizzle.js
+++ sizzle.js
@@ -1,6 +1,6 @@
 /*!
  * Sizzle CSS Selector Engine
- *  Copyright 2011, The Dojo Foundation
+ *  Copyright 2012, The Dojo Foundation
  *  Released under the MIT, BSD, and GPL Licenses.
  *  More information: http://sizzlejs.com/
  */
