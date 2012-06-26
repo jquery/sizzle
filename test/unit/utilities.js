4bcea9e4416066a284ae44d355aefed9a24a5c6e
Richard Gibson <richard.gibson@gmail.com>
2012-06-26 16:06:45 -0400

    copypasta cleanup

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -2,7 +2,6 @@ module("utilities", { teardown: moduleTeardown });
 
 test("contains", function() {
 	expect(15);
-	QUnit.reset();
 
 	var container = document.getElementById("nonnodes"),
 		element = container.firstChild,
@@ -12,16 +11,16 @@ test("contains", function() {
 	ok( element && element.nodeType === 1, "preliminary: found element" );
 	ok( text && text.nodeType === 3, "preliminary: found text" );
 	ok( nonContained, "preliminary: found non-descendant" );
-	ok( window.Sizzle.contains(container, element), "child" );
-	ok( window.Sizzle.contains(container.parentNode, element), "grandchild" );
-	ok( window.Sizzle.contains(container, text), "text child" );
-	ok( window.Sizzle.contains(container.parentNode, text), "text grandchild" );
-	ok( !window.Sizzle.contains(container, container), "self" );
-	ok( !window.Sizzle.contains(element, container), "parent" );
-	ok( !window.Sizzle.contains(container, nonContained), "non-descendant" );
-	ok( !window.Sizzle.contains(container, document), "document" );
-	ok( !window.Sizzle.contains(container, document.documentElement), "documentElement (negative)" );
-	ok( window.Sizzle.contains(document, document.documentElement), "documentElement (positive)" );
-	ok( window.Sizzle.contains(document, element), "document container (positive)" );
-	ok( !window.Sizzle.contains(document, detached), "document container (negative)" );
+	ok( Sizzle.contains(container, element), "child" );
+	ok( Sizzle.contains(container.parentNode, element), "grandchild" );
+	ok( Sizzle.contains(container, text), "text child" );
+	ok( Sizzle.contains(container.parentNode, text), "text grandchild" );
+	ok( !Sizzle.contains(container, container), "self" );
+	ok( !Sizzle.contains(element, container), "parent" );
+	ok( !Sizzle.contains(container, nonContained), "non-descendant" );
+	ok( !Sizzle.contains(container, document), "document" );
+	ok( !Sizzle.contains(container, document.documentElement), "documentElement (negative)" );
+	ok( Sizzle.contains(document, document.documentElement), "documentElement (positive)" );
+	ok( Sizzle.contains(document, element), "document container (positive)" );
+	ok( !Sizzle.contains(document, detached), "document container (negative)" );
 });
