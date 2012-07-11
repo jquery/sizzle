b54d43719eb35e229816dd09da9c12a52a772d54
Richard Gibson <richard.gibson@gmail.com>
2012-07-11 16:34:56 -0400

    Fix jQuery #12054: documents just have no class.
    No attributes to get, either. Closes gh-127.

--- sizzle.js
+++ sizzle.js
@@ -413,7 +413,7 @@ var Expr = Sizzle.selectors = {
 				}
 			}
 			return function( elem ) {
-				return pattern.test( elem.className || elem.getAttribute("class") || "" );
+				return pattern.test( elem.className || (elem.getAttribute && elem.getAttribute("class")) || "" );
 			};
 		},
 
