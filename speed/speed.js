59125c2526d81c5a7c473cbc1ea193d0fe959404
Timmy Willison <timmywillisn@gmail.com>
2012-09-10 10:03:41 -0400

    Adjust default selector engine list

--- speed/speed.js
+++ speed/speed.js
@@ -61,11 +61,11 @@ function( require, Benchmark, document, selectors ) {
 		engines = {
 			// "qsa":            "d.querySelectorAll( s )",
 			"jquery-1.7.2":   "jQuery.find( s, d )",
+			"jquery-1.8.1":   "jQuery.find( s, d )",
 			"sizzle":         "Sizzle( s, d )",
 			"dojo":           "dojo.query( s, d )",
 			"mootools-slick": "Slick.search( d, s )",
-			"nwmatcher":      "NW.Dom.select( s, d )",
-			"qwery":          "qwery( s, d )"
+			"nwmatcher":      "NW.Dom.select( s, d )"
 		},
 
 		// Keeps track of overall scores
