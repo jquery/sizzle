d1d0cf8aed8dd3bbfa557c5f25630c0e12cccf84
Richard Gibson <richard.gibson@gmail.com>
2013-02-17 09:32:03 -0500

    No ticket: Update copyright

--- sizzle.js
+++ sizzle.js
@@ -1,6 +1,6 @@
 /*!
  * Sizzle CSS Selector Engine
- * Copyright 2012 jQuery Foundation and other contributors
+ * Copyright 2013 jQuery Foundation and other contributors
  * Released under the MIT license
  * http://sizzlejs.com/
  */
