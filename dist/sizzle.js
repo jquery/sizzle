59ed9ec03d75f4774241c025758d01d8174a9ae9
Timmy Willison <timmywillisn@gmail.com>
2013-11-13 22:01:41 -0500

    Update version to 1.10.13-pre

--- dist/sizzle.js
+++ dist/sizzle.js
@@ -1,5 +1,5 @@
 /*!
- * Sizzle CSS Selector Engine v1.10.12
+ * Sizzle CSS Selector Engine v1.10.13-pre
  * http://sizzlejs.com/
  *
  * Copyright 2013 jQuery Foundation, Inc. and other contributors
