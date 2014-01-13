2b635881f7bceb25f8640d01be5ef0f7cf4a4028
Richard Gibson <richard.gibson@gmail.com>
2014-01-13 09:27:27 -0500

    Update version to 1.10.17-pre

--- dist/sizzle.js
+++ dist/sizzle.js
@@ -1,5 +1,5 @@
 /*!
- * Sizzle CSS Selector Engine v1.10.16
+ * Sizzle CSS Selector Engine v1.10.17-pre
  * http://sizzlejs.com/
  *
  * Copyright 2013 jQuery Foundation, Inc. and other contributors
