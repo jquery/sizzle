3e71fcf454c41e56d82921b6650136c2af908fd7
Michał Gołębiowski <m.goleb@gmail.com>
2014-02-05 15:05:21 +0100

    Update version to 1.10.19-pre

--- dist/sizzle.js
+++ dist/sizzle.js
@@ -1,5 +1,5 @@
 /*!
- * Sizzle CSS Selector Engine v1.10.18
+ * Sizzle CSS Selector Engine v1.10.19-pre
  * http://sizzlejs.com/
  *
  * Copyright 2013 jQuery Foundation, Inc. and other contributors
