72e4d9961db2cbaaf6fc3ad2ffa48e7ac9acd50f
Oleg Gaidarenko <markelog@gmail.com>
2014-03-31 22:48:44 +0400

    Build: Add more useful "test" task
    With it you can specify browsers you want to run tests.
    Example: grunt test --browsers=chrome-30,android_2.3

---
+++ tasks/test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = function( grunt ) {
+	grunt.registerTask( "test", "Run lint and tests for the specific browsers", function() {
+		var browsers = grunt.option( "browsers" ),
+			options = grunt.config.data.karma.unit;
+
+		if ( browsers ) {
+			options.browsers = browsers.split( "," );
+		}
+
+		grunt.task.run([ "lint", "karma:unit" ]);
+	});
+};
