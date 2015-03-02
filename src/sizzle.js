f171ebd7309ecd7c5a53c58527226d64b5941495
Jörn Wagner <joern.wagner@explicatis.com>
2015-03-02 13:47:41 +0100

    Make JSdoc return type of createCache comply with type syntax, so that Google Closure Compiler does not complain

--- src/sizzle.js
+++ src/sizzle.js
@@ -300,7 +300,7 @@ function Sizzle( selector, context, results, seed ) {
 
 /**
  * Create key-value caches of limited size
- * @returns {Function(string, Object)} Returns the Object data after storing it on itself with
+ * @returns {function(string, object)} Returns the Object data after storing it on itself with
  *	property name the (space-suffixed) string and (if the cache is larger than Expr.cacheLength)
  *	deleting the oldest entry
  */
