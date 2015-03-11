dd08a67d5ea2ebef8ccd3b544b7056068d120088
Colin Frick <colin@bash.li>
2015-03-11 07:20:12 +0100

    Test: Class selector on SVG Elements jquery/sizzle#322

--- test/unit/selector.js
+++ test/unit/selector.js
@@ -252,7 +252,7 @@ test("id", function() {
 });
 
 test("class", function() {
-	expect( 26 );
+	expect( 27 );
 
 	t( "Class Selector", ".blog", ["mark","simon"] );
 	t( "Class Selector", ".GROUPS", ["groups"] );
@@ -295,6 +295,10 @@ test("class", function() {
 
 	div = jQuery("<div><svg width='200' height='250' version='1.1' xmlns='http://www.w3.org/2000/svg'><rect x='10' y='10' width='30' height='30' class='foo'></rect></svg></div>")[0];
 	equal( Sizzle(".foo", div).length, 1, "Class selector against SVG" );
+
+	var svg = document.createElement("svg");
+	svg = jQuery("<svg width='200' height='250' version='1.1' xmlns='http://www.w3.org/2000/svg'><rect x='10' y='10' width='30' height='30' class='foo'></rect></svg>")[0];
+	equal( Sizzle(".foo", svg).length, 1, "Class selector on SVG Element" );
 });
 
 test("name", function() {
