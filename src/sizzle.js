c9f2b390c04f4270e7116bd1ce0c541464c5b775
Clément P <cpartiot@gmail.com>
2016-10-18 14:26:24 +0200

    Update sizzle.js

--- src/sizzle.js
+++ src/sizzle.js
@@ -307,7 +307,7 @@ function Sizzle( selector, context, results, seed ) {
 
 					// Capture the context ID, setting it first if necessary
 					if ( (nid = context.getAttribute( "id" )) ) {
-						nid = Sizzle.escape(nid)
+						nid = Sizzle.escape(nid);
 					} else {
 						context.setAttribute( "id", (nid = expando) );
 					}
