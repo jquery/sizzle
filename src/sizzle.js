3965b4ad93f7f901e4703846b40fa848a2f6c119
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2019-10-21 18:50:49 +0200

    Selector: Throw on post-comma invalid selectors in Opera 10-11 again
    PR gh-456 introduced a test catching not throwing on badly-escaped identifiers
    by Firefox 3.6-5. Unfortunately, it was placed just before a test Opera 10-11
    fails, making Opera fail quicker and not adding a post-comma invalid selector
    to rbuggyQSA.
    
    This commit fixes the regression & introduces a unit test ensuring the throwing.
    Unfortunately, the issue is fixed in Opera 11.6 which is the lowest one Sizzle
    officially supports but I verified the fix manually against Opera 10.6, 11.1
    & 11.5.
    
    Closes gh-463
    Ref gh-456

--- src/sizzle.js
+++ src/sizzle.js
@@ -840,6 +840,11 @@ setDocument = Sizzle.setDocument = function( node ) {
 			if ( !el.querySelectorAll( "a#" + expando + "+*" ).length ) {
 				rbuggyQSA.push( ".#.+[+~]" );
 			}
+
+			// Support: Firefox <=3.6 - 5 only
+			// Old Firefox doesn't throw on a badly-escaped identifier.
+			el.querySelectorAll( "\\\f" );
+			rbuggyQSA.push( "[\\r\\n\\f]" );
 		} );
 
 		assert( function( el ) {
@@ -870,11 +875,6 @@ setDocument = Sizzle.setDocument = function( node ) {
 			if ( el.querySelectorAll( ":disabled" ).length !== 2 ) {
 				rbuggyQSA.push( ":enabled", ":disabled" );
 			}
-
-			// Support: Firefox <=3.6 - 5 only
-			// Old Firefox doesn't throw on a badly-escaped identifier.
-			el.querySelectorAll( "\\\f" );
-			rbuggyQSA.push( "[\\r\\n\\f]" );
 
 			// Support: Opera 10 - 11 only
 			// Opera 10-11 does not throw on post-comma invalid pseudos
