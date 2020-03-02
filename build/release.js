096f6836d41d118cbf74a98a5b3cd899594d99a8
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-03-02 20:29:32 +0100

    Release: Make Sizzle work with jquery-release, remove generated files
    Running `grunt` will now just build Sizzle, do a size comparison & run ESLint,
    similarly to what we do in jQuery. `grunt test` is used to run unit tests and
    it requires running `grunt` or `grunt build` before to generate the built
    Sizzle version.
    
    Fixes gh-239
    Closes gh-467

--- build/release.js
+++ build/release.js
@@ -8,20 +8,13 @@ module.exports = function( Release ) {
 	];
 
 	Release.define( {
+		cdnPublish: false,
 		npmPublish: true,
 		issueTracker: "github",
 
 		generateArtifacts: function( done ) {
 			Release.exec( "grunt", "Grunt command failed" );
 			done( files );
-		},
-
-		// TODO Add Sizzle to the CDN, or add public opt-out interfaces to jquery-release
-		_copyCdnArtifacts: function() {
-			console.warn( "Skipping CDN artifact copy." );
-		},
-		_pushToCdn: function() {
-			console.warn( "Skipping CDN push." );
 		}
 	} );
 
